f9a29ac80f216f508eaa592bb2824f9f1428141f
sreezz-ai <85176770+sreezz-ai@users.noreply.github.com>
2022-02-06 08:25:51 -0500

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -12,59 +12,59 @@ let wk = Config.WORKTYPE == 'private' ? true : false
       
         var ttinullimage = await axios.get(`https://docs-jojo.herokuapp.com/api/randomloli`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg})
     
     }));
    
     
     Asena.addCommand({ pattern: 'wifu ?(.*)', fromMe: wk,dontaddCommandList: true }, (async (message, match) => {
 
-       var ttinullimage = await axios.get(`https://bx-${Config.ON}.herokuapp.com/api/sfw/waifu?apikey=${Config.CCN}`, { responseType: 'arraybuffer' })
+       var ttinullimage = await axios.get(`https://bx-hunter.herokuapp.com/api/sfw/waifu?apikey=Ikyy69`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg})
     
     }));
 
 
     Asena.addCommand({ pattern: 'awoo ?(.*)', fromMe: wk,dontaddCommandList: true }, (async (message, match) => {
         
-        var ttinullimage = await axios.get(`https://bx-${Config.ON}.herokuapp.com/api/sfw/awoo?apikey=${Config.CCN}`, { responseType: 'arraybuffer' })
+        var ttinullimage = await axios.get(`https://bx-hunter.herokuapp.com/api/sfw/awoo?apikey=Ikyy69`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg})
     
     }));   
     
     
     Asena.addCommand({ pattern: 'shinobu ?(.*)', fromMe: wk,dontaddCommandList: true }, (async (message, match) => {
         
-        var ttinullimage = await axios.get(`https://bx-${Config.ON}.herokuapp.com/api/sfw/shinobu?apikey=${Config.CCN}`, { responseType: 'arraybuffer' })
+        var ttinullimage = await axios.get(`https://bx-hunter.herokuapp.com/api/sfw/shinobu?apikey=Ikyy69`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg})
     
     }));
     
     Asena.addCommand({ pattern: 'hentai ?(.*)', fromMe: wk, onlyPm: true,dontaddCommandList: true }, (async (message, match) => {
       
-        var ttinullimage = await axios.get(`https://${Config.HLOCK}.herokuapp.com/api/hentai`, { responseType: 'arraybuffer' })
+        var ttinullimage = await axios.get(`https://hadi-api.herokuapp.com/api/hentai`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg})
     
     }));
     
     Asena.addCommand({ pattern: 'yuri ?(.*)', fromMe: wk, onlyPm: true,dontaddCommandList: true }, (async (message, match) => {
       
-        var ttinullimage = await axios.get(`https://${Config.HLOCK}.herokuapp.com/api/yuri`, { responseType: 'arraybuffer' })
+        var ttinullimage = await axios.get(`https://hadi-api.herokuapp.com/api/yuri`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg})
     
     }));
 
     Asena.addCommand({ pattern: 'megumin ?(.*)', fromMe: wk,dontaddCommandList: true }, (async (message, match) => {
         
 
-        var ttinullimage = await axios.get(`https://bx-${Config.ON}.herokuapp.com/api/sfw/megumin?apikey=${Config.CCN}`, { responseType: 'arraybuffer' })
+        var ttinullimage = await axios.get(`https://bx-hunter.herokuapp.com/api/sfw/megumin?apikey=Ikyy69`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg})
     
     }));
 
@@ -74,8 +74,8 @@ let wk = Config.WORKTYPE == 'private' ? true : false
 
     Asena.addCommand({ pattern: 'neko ?(.*)', fromMe: wk,dontaddCommandList: true }, (async (message, match) => {
 
-       var ttinullimage = await axios.get(`https://bx-${Config.ON}.herokuapp.com/api/sfw/neko?apikey=${Config.CCN}`, { responseType: 'arraybuffer' })
+       var ttinullimage = await axios.get(`https://bx-hunter.herokuapp.com/api/sfw/neko?apikey=Ikyy69`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg})
     
     }));
